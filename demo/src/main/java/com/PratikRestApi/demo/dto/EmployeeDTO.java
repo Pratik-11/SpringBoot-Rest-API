81340478dfc4f296cb39a963edd9e17c0a9bb69b
Pratik S <pratiksingh.sskr@gmail.com>
2024-07-31 00:11:58 +0530

    rest get

--- demo/src/main/java/com/PratikRestApi/demo/dto/EmployeeDTO.java
+++ demo/src/main/java/com/PratikRestApi/demo/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.PratikRestApi.demo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ public class EmployeeDTO {
     private Long id;
     private String name;
     private LocalDate dateOfJoining;
+    @JsonProperty("isActive")
     private boolean isActive;
 
 
